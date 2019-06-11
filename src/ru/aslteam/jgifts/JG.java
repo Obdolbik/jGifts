4aea7a69981c1e63ead4bd0fab54c9340a1e1721
obdolbanius <hulriker25@gmail.com>
2019-06-11 14:00:43 +0300

    Что-то с плагином

---
+++ src/ru/aslteam/jgifts/JG.java
@@ -0,0 +1,16 @@
+package ru.aslteam.jgifts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JG extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
